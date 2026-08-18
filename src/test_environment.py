230acbd363d5b7ee9acbcf5f213c2fb1ccc52ab1
Ramona <ramonaigb1@gmail.com>
2026-08-19 00:42:50 +0100

    Initial project setup

---
+++ src/test_environment.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib
+import sklearn
+from rdkit import Chem
+
+print("Pandas:", pd.__version__)
+print("NumPy:", np.__version__)
+print("matplotlib:", matplotlib.__version__)
+print("Scikit-learn:", sklearn.__version__)
+print("RDKit:", Chem.__version__)
+print("Environment Working!")
