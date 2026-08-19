8f58c29fc8173f353ab0364cce287950aef387a9
Ramona <ramonaigb1@gmail.com>
2026-08-19 16:29:59 +0100

    Add project dependencies

--- src/test_environment.py
+++ src/test_environment.py
@@ -2,11 +2,11 @@ import pandas as pd
 import numpy as np
 import matplotlib
 import sklearn
-from rdkit import Chem
+import rdkit
 
 print("Pandas:", pd.__version__)
 print("NumPy:", np.__version__)
 print("matplotlib:", matplotlib.__version__)
 print("Scikit-learn:", sklearn.__version__)
-print("RDKit:", Chem.__version__)
+print("RDKit:", rdkit.__version__)
 print("Environment Working!")
